ec5343218093814d630b0c481e7cfabd6011d6e2
luna.hortin@gmail.com <luna.hortin@gmail.com>
2023-05-22 15:34:24 +1000

    Another tree!

--- l-systems/main.js
+++ l-systems/main.js
@@ -11,23 +11,27 @@ ctx.fillRect(0,0,1000,1000)
 
 // b is branch
 // l is leaf
+// f is flower
 // ( is push state
 // ) is pop state
 // + is turn left
 // - is turn right
 let rules = {
   b: "b(+l)(--l)b", 
-  l: "b((+l)++l)-l"
+  l: "b((+l)++lf)(f-l)",
+  f: ""
 }
 
 // in radians
-let angle = 0.3
+let angle = 0.5
 let lineLength = 25
 
 let actions = {
   b() {
     ctx.beginPath()
-    ctx.strokeStyle = "white"
+    ctx.strokeStyle = "#a65"
+    ctx.lineWidth = 7
+    ctx.lineCap = "butt"
     ctx.moveTo(0,0)
     ctx.translate(0,-lineLength)
     ctx.lineTo(0,0)
@@ -36,10 +40,18 @@ let actions = {
   l() {
     ctx.beginPath()
     ctx.strokeStyle = "green"
+    ctx.lineWidth = 15
+    ctx.lineCap = "round"
     ctx.moveTo(0, 0)
     ctx.translate(0, -lineLength)
     ctx.lineTo(0, 0)
     ctx.stroke()
+  },
+  f() {
+    ctx.beginPath()
+    ctx.fillStyle = "pink"
+    ctx.arc(0,10,6,0,7)
+    ctx.fill()
   }, 
   "+": () => {
     ctx.rotate(-angle)
@@ -61,7 +73,7 @@ let string = "l"
 function iterate() {
   let chars = string.split("")
   let newChars = chars.map((char) => {
-    if (rules[char]) {
+    if (rules[char] !== undefined) {
       return rules[char]
     } else {
       return char
@@ -73,8 +85,6 @@ upto(5, iterate)
 
 function draw() {
   ctx.save()
-  ctx.strokeStyle = "white"
-  ctx.lineWidth = 5
   ctx.translate(500, 1000)
   
   string.split("").forEach((char) => {
