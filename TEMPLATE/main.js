d8ac30f9cc9fe8d4d42f578ca2a86f687f91def2
luna.hortin@gmail.com <luna.hortin@gmail.com>
2023-05-22 10:07:50 +1000

    Update template

--- TEMPLATE/main.js
+++ TEMPLATE/main.js
@@ -1,4 +1,9 @@
 // jQuery lite
 window.S = document.querySelector.bind(document);
 
+// Random shorthand
+function rand(x) {
+  return Math.random() * x
+}
+
 S('body').innerHTML = "<span id='test'>Working!</span>";
