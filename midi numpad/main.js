778817d5dc79cec37ab879da17b80abcdd27da5e
Luna Hortin <luna.hortin@gmail.com>
2024-05-09 16:59:02 +1000

    Midi numpad! :D Specifically for this random Targus numpad I have with backspace and tab keys

---
+++ midi numpad/main.js
@@ -0,0 +1,99 @@
+const keycodes = ["Enter", "NumpadAdd", "NumpadSubtract", "Backspace",
+    "Numpad3", "Numpad6", "Numpad9", "NumpadMultiply",
+    "Numpad2", "Numpad5", "Numpad8", "NumpadDivide",
+    "Numpad1", "Numpad4", "Numpad7", "Tab"
+    ]
+
+const octave = [
+  "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+]
+
+const keysPressed = new Map()
+
+const noteNames = []
+for (let i = 1; i <= 10; i++) {
+  for (let note of octave) {
+    noteNames.push(note + i)
+  }
+}
+
+let upPressed = false
+let downPressed = false
+const initialOffset = 64
+let offset = initialOffset
+
+function showOffset() {
+  S("#offset").innerHTML = offset
+}
+showOffset()
+
+function keyLocation(key) {
+  return keycodes.indexOf(key)
+}
+
+function locationToNote(loc) {
+  return offset + loc
+}
+
+document.addEventListener("keydown", (ev) => {
+  if (ev.repeat) return;
+
+  if (ev.code === "Numpad0") {
+    if (downPressed) {
+      offset = initialOffset
+      showOffset()
+      return
+    }
+    offset += 4
+    showOffset()
+    upPressed = true
+    return;
+  }
+
+  if (ev.code === "NumpadDecimal") {
+    if (upPressed) {
+      offset = initialOffset
+      showOffset()
+      return
+    }
+    offset -= 4
+    showOffset()
+    downPressed = true
+    return;
+  }
+
+  const loc = keyLocation(ev.code)
+  if (loc === -1) return;
+
+  const note = locationToNote(loc)
+
+  if (synth)
+    noteStart(note);
+  if (midi)
+    midiKeyDown(note);
+
+  keysPressed.set(ev.code, note)
+
+  ev.preventDefault()
+})
+
+document.addEventListener("keyup", (ev) => {
+  if (ev.code === "Numpad0") {
+    upPressed = false
+    return;
+  }
+
+  if (ev.code === "NumpadDecimal") {
+    downPressed = false
+    return;
+  }
+
+  const note = keysPressed.get(ev.code)
+  keysPressed.delete(ev.code)
+
+  if (synth)
+    noteEnd(note);
+  if (midi)
+    midiKeyUp(note);
+
+})
---
+++ midi numpad/midi.js
@@ -0,0 +1,30 @@
+let midi
+
+function startMidi() {
+  console.log("requesting midi access")
+  navigator.requestMIDIAccess().then((midiAccess) => {
+    console.log('midi acquired')
+    midi = midiAccess
+  }, () => {
+    console.log('access denied')
+  })
+}
+
+S('#midi').onclick = startMidi
+
+
+function midiKeyDown(note) {
+  midi.outputs.forEach((output) => {
+    console.log("sending note on for " + note)
+    console.log(output)
+    output.send([0x90, note + 24, 127])
+  })
+}
+
+function midiKeyUp(note) {
+  midi.outputs.forEach((output) => {
+    console.log("sending note off for " + note)
+    console.log(output)
+    output.send([0x80, note + 24, 127])
+  })
+}
